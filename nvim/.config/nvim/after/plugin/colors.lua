467b52be11fb61964a960f3199f7d6af1ab39785
Rahul Anil Damodaran <rahulanil101@gmail.com>
2023-02-05 20:35:08 +0530

    New turbo c++ theme added to the config for nvim

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,13 +1,13 @@
-
-function ColorMyPencils(color) 
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+function ColorMyPencils(color)
+    color = color or "rose-pine"
+    vim.cmd.colorscheme(color)
     vim.g.gruvbox_contrast_dark = "hard"
     vim.g.gruvbox_invert_selection = '0'
 
-	--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    --vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 end
 
-ColorMyPencils("gruvbox")
+--ColorMyPencils("gruvbox")
+ColorMyPencils("borlandp")
