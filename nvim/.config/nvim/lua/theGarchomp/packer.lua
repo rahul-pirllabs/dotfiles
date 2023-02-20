1ec68650498dbe4b93888b6b4c061140f14f879a
Rahul Anil Damodaran <rahulanil101@gmail.com>
2023-02-20 14:57:21 +0530

    Custom theme (my tango-dark work in progress)

--- nvim/.config/nvim/lua/theGarchomp/packer.lua
+++ nvim/.config/nvim/lua/theGarchomp/packer.lua
@@ -4,73 +4,87 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-        -- Packer can manage itself
-        use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-        use {
-            'nvim-telescope/telescope.nvim', tag = '0.1.0',
-            -- or                            , branch = '0.1.x',
-            requires = { { 'nvim-lua/plenary.nvim' } }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+    use { "nvim-telescope/telescope-file-browser.nvim" }
+
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+    })
+
+    use("gruvbox-community/gruvbox")
+    use({ "catppuccin/nvim", as = "catppuccin" })
+    -- use("vim-scripts/Tango-colour-scheme")
+    use("Rahul-Anil/tango-dark")
+    use("noahfrederick/vim-noctu")
+
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/playground')
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
         }
-        use { "nvim-telescope/telescope-file-browser.nvim" }
+    }
+    use('sbdchd/neoformat')
 
-        use({
-            'rose-pine/neovim',
-            as = 'rose-pine',
-        })
+    use('TimUntersberger/neogit')
 
-        use("gruvbox-community/gruvbox")
-        use({ "catppuccin/nvim", as = "catppuccin" })
-        use("vim-scripts/Tango-colour-scheme")
-        use("noahfrederick/vim-noctu")
+    -- auto pairs
+    use("windwp/nvim-autopairs")
+    -- auto tags
+    use("windwp/nvim-ts-autotag")
 
-        use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-        use('nvim-treesitter/playground')
-        use('theprimeagen/harpoon')
-        use('mbbill/undotree')
-        use('tpope/vim-fugitive')
+    -- Comments
+    use {
+        'numToStr/Comment.nvim',
+    }
 
-        use {
-            'VonHeikemen/lsp-zero.nvim',
-            requires = {
-                -- LSP Support
-                { 'neovim/nvim-lspconfig' },
-                { 'williamboman/mason.nvim' },
-                { 'williamboman/mason-lspconfig.nvim' },
+    -- Indent Lines
+    use("lukas-reineke/indent-blankline.nvim")
 
-                -- Autocompletion
-                { 'hrsh7th/nvim-cmp' },
-                { 'hrsh7th/cmp-buffer' },
-                { 'hrsh7th/cmp-path' },
-                { 'saadparwaiz1/cmp_luasnip' },
-                { 'hrsh7th/cmp-nvim-lsp' },
-                { 'hrsh7th/cmp-nvim-lua' },
+    -- Markdown preview
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function() vim.fn["mkdp#util#install"]() end,
+    })
 
-                -- Snippets
-                { 'L3MON4D3/LuaSnip' },
-                { 'rafamadriz/friendly-snippets' },
+    -- TODO: manager
+    use {
+        "folke/todo-comments.nvim",
+        requires = "nvim-lua/plenary.nvim",
+        config = function()
+            require("todo-comments").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
             }
-        }
-        use('sbdchd/neoformat')
-
-        use('TimUntersberger/neogit')
-
-        -- auto pairs
-        use("windwp/nvim-autopairs")
-        -- auto tags
-        use("windwp/nvim-ts-autotag")
-
-        -- Comments
-        use {
-            'numToStr/Comment.nvim',
-        }
-
-        -- Indent Lines
-        use("lukas-reineke/indent-blankline.nvim")
-
-        -- Markdown preview
-        use({
-            "iamcco/markdown-preview.nvim",
-            run = function() vim.fn["mkdp#util#install"]() end,
-        })
-    end)
+        end
+    }
+end)
